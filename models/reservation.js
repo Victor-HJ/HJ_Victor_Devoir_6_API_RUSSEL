66c6a0f51f8921b7eb6bd8ce86c8eedd43070db3
Vimmz <victorhuinjoseph@gmail.com>
2026-08-25 01:34:04 +0200

    models

---
+++ models/reservation.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Reservation = new Schema ({
+    catwayNumber : {
+        type : Number, 
+        required : [true, 'Un catway doit être associé à la réservation'],
+        ref : 'Catway'
+    },
+
+    clientName : {
+        type : String,
+        trim : true, 
+        required : [true, 'Un nom doit être associé à la réservation'],
+        minlength : [3, 'Le nom ne peut faire moins de 3 caractères']
+    },
+
+    boatName : {
+        type: String, 
+        trim : true,
+        required : [true, 'Un nom de navire doit être associé à la réservation'],
+        minlenght : [3, 'Le nom du navire doit faire au moins 3 caractères']
+    },
+
+    startDate : {
+        type : Date,
+        required : [true, 'Une date de début de réservation est requise']
+    },
+
+    endDate : {
+        type : Date,
+        required : [true, 'Une date de fin de réservation est requise'],
+        validate : {
+            validator : function(date) {
+                return date > this.startDate;
+            },
+            message : 'La date de fin ne peut être antérieure à la date de début'
+        }
+    }
+}, {
+
+})
